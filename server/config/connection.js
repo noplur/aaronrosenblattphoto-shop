735969f08cc20dcd53ca1459705408d445c2442b
noplur <geoduckphoto@gmail.com>
2021-11-28 12:20:17 -0800

    connection change

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/rosenblattphotoshop', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/rosenblattphoto_shop', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
